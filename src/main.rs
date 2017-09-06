9c725feffe6ddccdf31da06f30767d9462df8de9
Stefano Chiodino <louderth@gmail.com>
2017-09-06 20:09:01 +0100

    update and cleanup CI config

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
 mod models;
 use models::direction;
 
-fn main() {
-    let a: models::feature::Feature = models::feature::Feature::Monastery;
-
-    match a {
-        models::feature::Feature::Road { connections } => println!("yes"),
-        _ => panic!("dad"),
-    }
-}
+fn main() {}
